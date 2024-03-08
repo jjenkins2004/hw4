f9825526f2f8f864a09e37049d00e1c137096865
Joshua Jenkins <2004joshuaj@gmail.com>
2024-03-08 12:06:37 -0800

    equal-paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,40 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int mindepth(Node * root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int leftdepth = mindepth(root->left);
+    int rightdepth = mindepth(root->right);
+
+    if (leftdepth != 0 && leftdepth < rightdepth) {
+        return leftdepth+1;
+    } else if (rightdepth != 0){
+        return rightdepth+1;
+    } else {
+        return leftdepth+1;
+    }
+}
+
+int maxdepth(Node * root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    int leftdepth = maxdepth(root->left);
+    int rightdepth = maxdepth(root->right);
+
+    if (leftdepth > rightdepth) {
+        return leftdepth+1;
+    } else {
+        return rightdepth+1;
+    }
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    return mindepth(root) == maxdepth(root);
 
 }
 
